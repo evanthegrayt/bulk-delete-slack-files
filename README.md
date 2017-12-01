b0639d0735f45c7eb3b69afea8593fca8640d9f2
Evan Gray <evan.gray@adfitech.com>
2017-12-01 10:01:09 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # delete-multiple-slack-files
 Ruby script that utilizes the Slack API to delete multiple files on Slack
+
+### Getting Started
+- Get your token from https://api.slack.com/custom-integrations/legacy-tokens
+- Set your name and token (search for TODO)
+- rvm use ruby-2.2.1
+- gem install slack-api
+
+### Usage
+- Add the repository's bin to your `PATH`, or better yet, link it.
+- Run this with -h for help, -t for test, or no opts to actually delete.
+
---
+++ lib/slack_file_deleter.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+require 'slack'
+require 'optparse'
+
+class SlackFileDeleter
+  def initialize(name, token, opts = {})
+    @name   = name
+    @client = Slack::Client.new(token: token)
+    @opts   = opts
+
+    @opts[:test]  ||= false
+    @opts[:count] ||= 1000
+    @opts[:days]  ||= 30
+  end
+
+  def delete_all_old_files
+    del_count = 0
+    file_list.each do |file|
+      if meets_requirements?(file)
+        puts "#{delete_word}: [#{file['name']}]"
+        delete_file(file) unless @opts[:test]
+        del_count += 1
+      end
+    end
+    puts "\n#{del_count} Files #{delete_word}."
+  end
+
+  private
+
+  def delete_file(file)
+    @client.files_delete(file: file['id'])
+  end
+
+  def file_list
+    @client.files_list(count: @opts[:count])['files']
+  end
+
+  def meets_requirements?(file)
+    file['created'] < date.to_i && file['user'] == uid
+  end
+
+  def uid
+    @uid ||= @client.users_list['members'].select do |m|
+      m['profile']['real_name'] == @name
+    end.first['id']
+  end
+
+  def delete_word
+    @opts[:test] ? 'To Delete' : 'Deleted'
+  end
+
+  def date
+    Time.now - @opts[:days] * 24 * 60 * 60
+  end
+
+end
+
